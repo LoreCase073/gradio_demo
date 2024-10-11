ea72ea0373dffd5f534b3ed57f3dc2d3effb6874
Lore <lorecaselli00@gmail.com>
2024-10-11 11:25:58 +0200

    - Updated x_clip_main.py to include the necessary code for running inferences with xclip.

--- x_clip_main.py
+++ x_clip_main.py
@@ -5,40 +5,21 @@ from methods.summarizer import Summarizer
 
 
 def main(file_path: str, method_name: str, file_name: str, out_path: str, exp_name: str, video_folder:str,
-         input_prompt: str, max_length: int = 100, no_creation: bool=False, image_in: bool=False, device_id: int=0,
+         input_prompt: list, max_length: int = 100, device_id: int=0,
          num_frames: int=10, frame_rate: int=3) -> None:
 
-    if method_name == 'llava_next_video':
-        approach = LlavaNextVideo(model_type=method_name, device=device_id, model_savepath='', 
-                                  output_path=out_path, experiment_name=exp_name, video_folder=video_folder, 
-                                  num_frames=num_frames, frame_rate=frame_rate)
-        prompt = [
-                    {
-
-                        "role": "user",
-                        "content": [
-                            {"type": "text", "text": "{}".format(input_prompt)},
-                            {"type": "video"},
-                            ],
-                    },
-                ]
-    elif method_name == 'xclip':
+    if method_name == 'xclip':
         approach = XCLIP(model_type=method_name, device=device_id, model_savepath='', 
                          output_path=out_path, experiment_name=exp_name, video_folder=video_folder, 
                          num_frames=num_frames, frame_rate=frame_rate)
-        prompt = ['stopped vehicle', 'normal traffic']
+        prompt = input_prompt
     else:
         print('This method is not yet implemented')
         return 
     
-    if image_in:
-        approach.caption_image_file(file_path=file_path, image_name=file_name, prompt=prompt, no_creation=no_creation, max_length=max_length)
-    else:
-        csv_path_file, prompt_to_delete = approach.caption_video_file(file_path=file_path, video_name=file_name, prompt=prompt, no_creation=no_creation, max_length=max_length)
-        if method_name != 'xclip':
-            # summarize the video output
-            summarizer = Summarizer(out_path,exp_name)
-            summarizer.summarize_to_csv(file_name,csv_path_file,prompt_to_delete)
+    
+    _, _ = approach.caption_video_file(file_path=file_path, video_name=file_name, prompt=prompt, max_length=max_length)
+        
 
 
 if __name__ == "__main__":
@@ -58,12 +39,8 @@ if __name__ == "__main__":
                    help=('name of the video folder.'))
     p.add_argument('--max_length', type=int, default=100,
                    help=('Prompt to be fed to the method.'))
-    p.add_argument('--prompt', type=str,
+    p.add_argument('--prompt', nargs='+', type=str, required=True,
                    help=('Prompt to be fed to the method.'))
-    p.add_argument('--no_creation', type=str, choices=['yes', 'no'],
-                   help=('NoCreation.'))
-    p.add_argument('--image', type=str, choices=['yes', 'no'], default='no',
-                   help=('If to prompt an image.'))
     p.add_argument('--num_frames', type=int, default=10,
                    help=('How many frames to do inference with.'))
     p.add_argument('--frame_rate', type=int, default=3,
@@ -71,18 +48,8 @@ if __name__ == "__main__":
     
     args = p.parse_args()
 
-    if args.no_creation == 'yes':
-        no_creation = True
-    else:
-        no_creation = False
-
-    if args.image == 'yes':
-        image_in = True
-    else:
-        image_in = False
-
 
     main(file_path=args.file_path, method_name=args.method_name, file_name=args.file_name, out_path=args.out_path, 
          exp_name=args.exp_name, video_folder=args.video_folder, input_prompt=args.prompt,
-         max_length=args.max_length, no_creation=no_creation, image_in=image_in, device_id=0,
+         max_length=args.max_length, device_id=0,
          num_frames=args.num_frames, frame_rate=args.frame_rate)
